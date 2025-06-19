f0fcd6e00833fdfb0e82878c3fabb78b529c3a35
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-06-20 00:10:59 +0100

    Implement getComputerChoice function and update console log

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,14 @@
-console.log('hello')
+// console.log('hello')
+
+function getComputerChoice() {
+  let computerChoice = Math.random();
+  if (computerChoice <= 1/3 ) {
+    return 'rock';
+  } else if (computerChoice > 1/3 && computerChoice <= 2/3) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+console.log(getComputerChoice());
